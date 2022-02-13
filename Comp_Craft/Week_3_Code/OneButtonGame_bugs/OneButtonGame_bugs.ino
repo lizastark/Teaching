d7fbadd2fb2b58df06974ee1eb874f69173c8199
Liza Stark <lizastark@gmail.com>
2022-02-13 17:33:06 -0500

    updated buggy code

--- Comp_Craft/Week_3_Code/OneButtonGame_bugs/OneButtonGame_bugs.ino
+++ Comp_Craft/Week_3_Code/OneButtonGame_bugs/OneButtonGame_bugs.ino
@@ -3,6 +3,8 @@
    by rowan07
    https://create.arduino.cc/projecthub/rowan07/a-simple-reflex-game-937472
 
+   Note that there are bugs in this sketch that you need to solve.
+   There are 2 syntax bugs and one logic bug.
 */
 
 int ledPin = 13;
@@ -12,41 +14,43 @@ int sensorValue = 0;
 float realTime
 
 void setup() {
+  Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
   pinMode(sensorPin1, INPUT);
 }
 
 void loop() {
-  Serial.begin(9600);
   Serial.println("");
   Serial.println("");
   Serial.println("");
   Serial.println("Push the button to start game");
 
   while (digitalRead(sensorPin1) == 0) {
-  }
-  
-  Serial.println("");
-  Serial.println("");
-  Serial.println("");
-  Serial.println("Get Ready!");
-  delay(1000);
-  Serial.println("Get Set!");
-  delay(1000);
-  ranDelay = random(5000);
-  delay(ranDelay);
-  Serial.println("Go!");
+    Serial.println("");
+    Serial.println("");
+    Serial.println("");
+    Serial.println("Get Ready!");
+    delay(1000);
+    Serial.println("Get Set!");
+    delay(1000);
+    ranDelay = random(5000);
+    delay(ranDelay);
+    Serial.println("Go!");
 
-  realTime = millis();
-  digitalWrite(ledPin, HIGH);
+    realTime = millis();
+    digitalWrite(ledPin, HIGH);
+  }
+
+
 
 
   while (digitalRead(sensorPin1) == 0) {
   }
-  
-  digitalWrite(ledPin, LOW);
+
+  digitalWrite(ledPin, LOW;
   Serial.println("Your time was");
   realTime = millis() - realTime;
   Serial.print(realTime / 1000, 2);
   Serial.println(" seconds");
   delay(2000);
+}
